224e09b732c8065c352199b6cf45ed5f19fe9659
JqyModi <1294432350@qq.com>
2017-11-03 17:31:22 +0800

    first

---
+++ Psychologist/PsychologistViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by mac on 2017/11/3.
+//  Copyright © 2017年 modi. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+    //准备Segue
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
+
